2df3c2b2e3244afb2a0bb168eafcdd6295822d0f
Erik Sundberg <erik.emanuel.sundberg@outlook.com>
2023-04-04 22:07:21 +0200

    do bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,18 @@
 export default function bubble_sort(arr: number[]): void {
+    let canSwap;
+    do {
+        canSwap = false;
+        for (let i = 0; i < arr.length; i++) {
+            const current = arr[i],
+                next = arr[i + 1];
+            if (current > next) doSwap(arr, i);
+        }
+    } while (canSwap);
 
+    function doSwap(arr: number[], i: number) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        canSwap = true;
+    }
 }
